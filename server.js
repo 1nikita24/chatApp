6678fcc210c5e1c77c5e345e79ceeaea7bfc66c2
1niita24 <nikitaezhov@icloud.com>
2019-08-07 20:36:10 -0400

    heroku ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
+const PORT = process.env.PORT || 3000;
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
@@ -62,7 +63,4 @@ io.on('connection', () => {
 mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
     console.log('mongodb connected', err);
 })
-
-var server = http.listen(3000, () => {
-    console.log('server is running on port', server.address().port);
-});
+app.listen(PORT, () => console.log(`You are now on localhost:${PORT}.`));
